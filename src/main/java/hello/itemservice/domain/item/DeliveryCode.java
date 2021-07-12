1abd60de3633ae9f484d8c49e86b027703589a79
minseo23 <neverdie4757@gmail.com>
2021-07-12 22:47:35 +0900

    first commit

---
+++ src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -0,0 +1,17 @@
+package hello.itemservice.domain.item;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+/**
+ * FAST: 빠른 배송
+ * NORMAL: 일반 배송
+ * SLOW: 느린 배송
+ */
+public class DeliveryCode {
+    
+    private String code;
+    private String displayName;
+}
